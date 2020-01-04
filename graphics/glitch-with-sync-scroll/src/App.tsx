6a3d0f70b38b83d74587296ca4de490eb134da2c
30in2020 <30in2020@gmail.com>
2020-01-05 08:38:44 +0900

    Feat: add react-three-fiber, three module
    install react-three-fiber and three module.
    add typescript starter code for react-three-fiber.
    (Reference: https://codesandbox.io/s/react-three-fiber-ho4i8?from-embed)

--- graphics/glitch-with-sync-scroll/src/App.tsx
+++ graphics/glitch-with-sync-scroll/src/App.tsx
@@ -1,6 +1,32 @@
-import React from "react";
+import React, { useRef } from "react";
+import { render } from "react-dom";
+import { Canvas, useFrame } from "react-three-fiber";
+import * as THREE from "three";
+
+const Box: React.FC = () => {
+  const ref = useRef<THREE.Mesh>();
+  useFrame(() => {
+    if (ref.current) {
+      ref.current.rotation.x += 0.005;
+      ref.current.rotation.y += 0.0075;
+    }
+  });
+  return (
+    <mesh ref={ref}>
+      <boxGeometry attach="geometry" args={[1, 1]} />
+      <meshStandardMaterial attach="material" color={0xfe9966} />
+    </mesh>
+  );
+};
+
 const App: React.FC = () => {
-  return <div className="App">Hello Doodles</div>;
+  return (
+    <Canvas>
+      <ambientLight />
+      <pointLight position={[10, 0, 10]} intensity={1} />
+      <Box />
+    </Canvas>
+  );
 };
 
 export default App;
