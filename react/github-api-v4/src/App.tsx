ea6b121bb5a2ea47432ea3ce4d1af7ce01c41750
30in2020 <30in2020@gmail.com>
2020-01-12 08:50:28 +0900

    Feat: create github-api-v4 directory

---
+++ react/github-api-v4/src/App.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn Github API
+        </a>
+      </header>
+    </div>
+  );
+};
+
+export default App;
