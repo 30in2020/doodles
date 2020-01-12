eccd656ee384a906b91ecf3c227bdbe2b4d833c8
30in2020 <30in2020@gmail.com>
2020-01-13 02:13:55 +0900

    Feat: add Github GraphQL API fetch samples
    Added some Github GraphQL API's fetch samples.
    - Get pinned repo
    - Get information of '365daysofcommit' repo
    - Get commit history of 'doodles' repo
    - Get PR list

--- react/github-api-v4/src/App.tsx
+++ react/github-api-v4/src/App.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Fetch from "./Fetch";
 
 const App: React.FC = () => {
   return (
@@ -13,8 +14,94 @@ const App: React.FC = () => {
           Learn Github API
         </a>
       </header>
+      <Fetch
+        label="Get my pinned repositories"
+        query={`
+          query {
+            viewer {
+              pinnedRepositories(first: 10, affiliations: [OWNER]) {
+                nodes{
+                  id
+                  nameWithOwner
+                }
+              }
+            }
+          }
+        `}
+      />
+      <Fetch
+        label="Get '365daysofcommit' repository's information"
+        query={`
+          query {
+            viewer {
+              id
+              bio
+              email
+              repository(name: "365daysofcommit") {
+                name
+                description 
+                url
+              }
+            }
+          }
+        `}
+      />
+      <Fetch
+        label="Get 'doodles' repository's commit history"
+        query={`
+          query {
+            viewer {
+              repository(name: "doodles") { 
+                languages(first: 10) {
+                  edges {
+                    node {
+                      color
+                      id
+                      name
+                    }
+                  }
+                }
+                object(expression: "master") {
+                  ... on Commit {
+                    history {
+                      totalCount
+                      nodes {
+                        committedDate
+                        message
+                        changedFiles
+                        oid
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        `}
+      />
+      <Fetch
+        label="Get PR list"
+        query={`
+        query {
+          viewer {
+            pullRequests(first: 10) {
+              nodes {
+                id
+                title
+                repository {
+                  id
+                  nameWithOwner
+                }
+              }
+            }
+          }
+        }
+        `}
+      />
     </div>
   );
 };
+
+// It seems there is no API for getting patch history of commit.
 
 export default App;
--- react/github-api-v4/src/Fetch.tsx
+++ react/github-api-v4/src/Fetch.tsx
@@ -1,64 +1,29 @@
 import React from "react";
 import useSWR from "swr";
 
-interface IPost {
-  data: {
-    post: {
-      id: string;
-      title: string;
-      body: string;
-    };
-  };
+console.log(process.env.REACT_APP_API_TOKEN);
+
+interface IProps {
+  query: string;
+  label: string;
 }
 
-const initialData: IPost = {
-  data: {
-    post: {
-      id: "",
-      title: "",
-      body: ""
-    }
-  }
-};
-
-const Fetch: React.FC = () => {
-  const { isValidating, data = initialData, error } = useSWR<IPost>(
-    `{
-      Query {
-        query {
-          repository(owner:"octocat", name:"Hello-World") {
-            issues(last:20, states:CLOSED) {
-              edges {
-                node {
-                  title
-                  url
-                  labels(first:5) {
-                    edges {
-                      node {
-                        name
-                      }
-                    }
-                  }
-                }
-              }
-            }
-          }
-        }
-      }
-    }`,
+const Fetch: React.FC<IProps> = ({ query, label }) => {
+  const { isValidating, data, error } = useSWR(
+    query,
     (query: string) =>
       fetch(`https://api.github.com/graphql`, {
         method: "POST",
         headers: {
-          Authorization: `bearer ${process.env.API_TOKEN}`
+          Authorization: `bearer ${process.env.REACT_APP_API_TOKEN}`
         },
         body: JSON.stringify({
-          query
+          query: query
         })
       }).then((res: Response) => res.json()),
     {
       // you can also override the global option
-      refreshInterval: 3000
+      //refreshInterval: 3000
     }
   );
 
@@ -67,10 +32,13 @@ const Fetch: React.FC = () => {
   }
 
   return (
-    <div style={{ height: 150 }}>
-      <h3>GraphQL Fetch</h3>
-      <p>refreshInterval: 3000</p>
-      {isValidating ? <div>loading...</div> : JSON.stringify(data.data)}
+    <div>
+      <h3>{label}</h3>
+      {isValidating || !data ? (
+        <div>loading...</div>
+      ) : (
+        <pre>{JSON.stringify(data.data, null, 2)}</pre>
+      )}
     </div>
   );
 };
