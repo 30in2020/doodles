1f6f0e0e0220fcf8ea0d599ae0365771f375c4d6
30in2020 <30in2020@gmail.com>
2020-01-19 08:43:59 +0900

    Feat: add github authentication code
    make it possible to get access token by just clicking the login button

---
+++ server/github-oauth/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+const axios = require("axios");
+const env = require("dotenv").config({ path: __dirname + "/.env" }).parsed;
+
+const clientID = env.CLIENT_ID;
+const clientSecret = env.CLIENT_SECRET;
+
+app.get("/callback", (req, res) => {
+  const requestToken = req.query.code;
+
+  axios({
+    method: "post",
+    url: `https://github.com/login/oauth/access_token?client_id=${clientID}&client_secret=${clientSecret}&code=${requestToken}`,
+    headers: {
+      accept: "application/json"
+    }
+  }).then(response => {
+    const accessToken = response.data.access_token;
+    console.log(response.data);
+    res.redirect(`/callback/?access_token=${accessToken}`);
+  });
+});
+
+app.use(express.static(__dirname + "/public"));
+app.listen(3000, () => {
+  console.log("Server listening on port : 3000");
+});
